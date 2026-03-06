e3246546f2940fc78967a5fa797e8e07c6700db3
Mihir Karandikar <mihirkkarandikar@gmail.com>
2026-03-06 08:57:33 +0530

    asdasD

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -18,11 +18,18 @@ int main()
     Car* car = new Car(500, 300, 30, 30);
 
     sf::RectangleShape carShape({car->getWidth(), car->getHeight()});
-    carShape.setPosition({car->getX(), car->getY()});
     carShape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
-    {
+    {   
+        /*
+            1. Process Events (things that happen once)
+            2. Handle Continuous Input
+            3. Update Game State
+            4. Render
+        */
+
+        // 1. Process Events
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>()) {
@@ -30,6 +37,24 @@ int main()
             }
         }
 
+        // 2. Handle Continuous Input
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            car->rotateAntiClockwise(1);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            car->rotateClockwise(1);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+            car->accelerateForward();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+            car->accelerateBackward();
+
+        // 3. Update Game State
+        car->move(1.0 / 60.0);
+        carShape.setPosition({car->getX(), car->getY()});
+
+        // 4. Render
         window.clear(sf::Color::Black);
 
         window.draw(carShape);
